c5976b2255e057b993d22174a9d8dadd10a8750c
Kleber <lyuri1508@gmail.com>
2024-06-08 17:44:09 -0300

    update favorite page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -57,30 +57,35 @@ const Favorites = () => {
     return (
         <div className="container">
             <h2 className="my-4">Favorite Posts</h2>
-            <div className="row">
-                {favoritePosts.map((post, index) => (
-                    <div key={index} className="col-md-6">
-                        <div className="card mb-3" style={{ cursor: 'pointer' }} onClick={() => handlePostClick(post.id)}>
-                            <div className="card-body">
-                                <div className="d-flex align-items-center mb-2">
-                                    <Image src={post.user.photoUrl} className="me-3" alt="User Thumbnail" roundedCircle style={{ width: '40px', height: '40px' }} />
-                                    <div>
-                                        <h5 className="card-title">{post.user.name}</h5>
+            {favoritePosts.length > 0 ? (
+                <div className="row">
+                    {favoritePosts.map((post, index) => (
+                        <div key={index} className="col-md-6">
+                            <div className="card mb-3" style={{ cursor: 'pointer' }} onClick={() => handlePostClick(post.id)}>
+                                <div className="card-body">
+                                    <div className="d-flex align-items-center mb-2">
+                                        <Image src={post.user.photoUrl} className="me-3" alt="User Thumbnail" roundedCircle style={{ width: '40px', height: '40px' }} />
+                                        <div>
+                                            <h5 className="card-title">{post.user.name}</h5>
+                                        </div>
                                     </div>
+                                    <h5 className="card-title">{post.title}</h5>
+                                    <p className="card-text">{post.content.slice(0, 100)}</p>
+                                    <p className="card-text">
+                                        <small className="text-muted">{formatDistanceToNow(new Date(post.createdAt.toDate()))} ago</small>
+                                        {post.interests.slice(0, 2).map((interest, index) => (
+                                            <span key={index} className="badge bg-secondary ms-2">{interest}</span>
+                                        ))}
+                                    </p>
                                 </div>
-                                <h5 className="card-title">{post.title}</h5>
-                                <p className="card-text">{post.content.slice(0, 100)}</p>
-                                <p className="card-text">
-                                    <small className="text-muted">{formatDistanceToNow(new Date(post.createdAt.toDate()))} ago</small>
-                                    {post.interests.slice(0, 2).map((interest, index) => (
-                                        <span key={index} className="badge bg-secondary ms-2">{interest}</span>
-                                    ))}
-                                </p>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            ) : (
+                <div>não há post favoritado.</div>
+            )}
+
         </div>
     )
 }
